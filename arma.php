eb06b829d7b8598e91a2f1fb9a29b74cfb194fb7
pityalvarez <97750378+pityalvarez@users.noreply.github.com>
2026-06-09 19:27:15 -0300

    saludos, esto es arma

---
+++ arma.php
@@ -0,0 +1,85 @@
+<?php
+// id
+// nombre
+// tipo
+// danioBase
+// nivelMinimo
+// estado (disponible, equipada, rota)
+
+// calcularDanio()
+// puedeSerEquipadaPor(Personaje $personaje)
+
+// Un arma rota no puede equiparse.
+// Un personaje debe cumplir el nivel mínimo para equipar un arma.
+// Un arma equipada no puede asignarse a otro personaje
+require_once 'config.php';
+class Arma {
+	private $db;
+
+	// Propiedades mapeadas directamente a tu DB
+	private int $id;
+	private string $nombre;
+	private string $tipo;
+	private int $danioBase;
+	private int $nivelMinimo;
+	private string $estado;
+
+	public function __construct() {
+	        $this->db = new Medoo([
+			    'type' => 'mariadb',
+			    'host' => 'localhost',
+			    'database' => 'biblioteca',
+			    'username' => 'root',
+			    'password' => ''
+	        ]);
+	}
+	/*public function getId(){
+		return $this->id;
+	}
+	public function setId($id){
+		$this->id = $id;
+	}
+	public function getNombre(){
+		return $this->nombre;
+	}
+	public function setNombre($nombre){
+		$this->nombre = $nombre;
+	}
+	public function getTipo(){
+		return $this->tipo;
+	}
+	public function setTipo($tipo){
+		$this->tipo = $tipo;
+	}*/
+	public function getDanioBase(){
+		return $this->danioBase;
+	}
+	/*public function setDanioBase($danioBase){
+		$this->danioBase = $danioBase;
+	}
+	public function getNivelMinimo(){
+		return $this->nivelMinimo;
+	}
+	public function setNivelMinimo($nivelMinimo){
+		$this->nivelMinimo = $nivelMinimo;
+	}
+	public function getEstado(){
+		return $this->estado;
+	}
+	public function setEstado($estado){
+		$this->estado = $estado;
+	}*/
+	// metodos
+	public function calcularDanio(){
+		return $this->getDanioBase();
+	}
+	public function puedeSerEquipadaPor(Personaje $personaje){
+		$bandera=false;
+        if ($this->estado === 'disponible'){
+        	if($personaje->getNivel() > $this->nivelMinimo){
+        		$bandera=true;
+        	}
+        }
+        return $bandera;
+	}
+}
