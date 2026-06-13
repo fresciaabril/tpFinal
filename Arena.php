4ea5e2971679e1919a4764ebd0dfd4ffbcc4e9c5
Santiago <santiagodominguez0271@gmail.com>
2026-06-12 23:15:46 -0300

    Termine metodo calcularModificadorArena

--- Arena.php
+++ Arena.php
@@ -1,16 +1,17 @@
 <?php
+include "Personaje.php";
+class Arena
+{
 
-class Arena {
-        
         //Atributos clase
         private $id;
         private $nombre;
         private $dificultad;
         private $capacidadPublico;
-        private $clima;
+        private $clima; //Normal / LLuvia / tormenta / niebla
 
         //Construct
-        public function __construct( $nombre, $dificultad, $capacidadPublico, $clima, $id=null)
+        public function __construct($nombre, $dificultad, $capacidadPublico, $clima, $id = null)
         {
                 $this->id = $id;
                 $this->nombre = $nombre;
@@ -20,48 +21,99 @@ class Arena {
         }
 
         //getters
-        public function getId(){
+        public function getId()
+        {
                 return $this->id;
         }
-        public function getNombre(){
+        public function getNombre()
+        {
                 return $this->nombre;
         }
-        public function getDificultad(){
+        public function getDificultad()
+        {
                 return $this->dificultad;
         }
-        public function getCapacidadPublico(){
+        public function getCapacidadPublico()
+        {
                 return $this->capacidadPublico;
         }
-        public function getClima(){
+        public function getClima()
+        {
                 return $this->clima;
         }
 
         //setters
-        public function setId($id){
+        public function setId($id)
+        {
                 $this->id = $id;
         }
-        public function setNombre($nombre){
+        public function setNombre($nombre)
+        {
                 $this->nombre = $nombre;
         }
-        public function setDificultad($dificultad){
+        public function setDificultad($dificultad)
+        {
                 $this->dificultad = $dificultad;
         }
-        public function setCapacidadPublico($capacidadPublico){
+        public function setCapacidadPublico($capacidadPublico)
+        {
                 $this->capacidadPublico = $capacidadPublico;
         }
-        public function setClima($clima){
+        public function setClima($clima)
+        {
                 $this->clima = $clima;
         }
 
         //Too string
         public function __toString()
         {
-                $respuesta = "Nombre de arena = {$this->getNombre()} \n Dificultad de arena = {$this->getDificultad()} \n Capacidad = {$this->getCapacidadPublico()} \n Clima = {$this->getClima()}";
+                $respuesta =
+                        "--------------------------------------------------- \n
+                Nombre de arena = {$this->getNombre()} \n 
+                Dificultad de arena = {$this->getDificultad()} \n 
+                Capacidad = {$this->getCapacidadPublico()} \n 
+                Clima = {$this->getClima()} \n
+                --------------------------------------------------- \n";
                 return $respuesta;
         }
 
         //Metodo calcular modificador de arena (Personaje $personaje)
-        public function calcularModificadorArena($objPersonaje){
-                
+        public function calcularModificadorArena(Personaje $objPersonaje)
+        {
+                $modificador = 0;
+                switch ($this->getClima())
+                {
+                        case "lluvia":
+                                if ($objPersonaje instanceof Arquero) {
+                                        $modificador = -10;
+                                } else if ($objPersonaje instanceof Mago) {
+                                        $modificador = 5;
+                                }
+                                // Guerrero queda en 0
+                                break;
+
+                        case "tormenta":
+                                if ($objPersonaje instanceof Mago) {
+                                        $modificador = 15;
+                                } else if ($objPersonaje instanceof Arquero) {
+                                        $modificador = -5;
+                                } else if ($objPersonaje instanceof Guerrero) {
+                                        $modificador = -5;
+                                }
+                                break;
+
+                        case "niebla":
+                                if ($objPersonaje instanceof Arquero) {
+                                        $modificador = -15;
+                                } else if ($objPersonaje instanceof Guerrero) {
+                                        $modificador = 5;
+                                }
+                                // Mago queda en 0
+                                break;
+
+                                // clima "normal": no entra a ningun case y devuelve 0
+                }
+
+                return $modificador;
         }
 }
