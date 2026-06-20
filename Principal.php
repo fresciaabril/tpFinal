300b74234963f497e7710f007871a0a0463d9173
fresciaabril <lizarragafrescia@gmail>
2026-06-20 01:51:24 -0300

    UN SOLO MENU

--- Principal.php
+++ Principal.php
@@ -47,7 +47,8 @@ MENU:
 			return $numero;
 		}
 		*/
-		echo"1. Listar todos los personajes.\n2. Listar personajes disponibles para duelar.\n3. Listar personajes lesionados.\n4. Listar personajes retirados.\n5. Listar armas disponibles.\n6. Mostrar el arma equipada por cada personaje.\n7. Mostrar todos los duelos realizados.\n8. Mostrar todos los duelos pendientes.\n9. Mostrar el historial de duelos de un personaje.\n10. Mostrar el ranking de personajes ordenado por cantidad de victorias.\n11. Mostrar el personaje con mayor cantidad de victorias.\n12. Mostrar el porcentaje de victorias de cada personaje.\n13. Mostrar la arena donde más duelos se realizaron.\n";
+		
+		
 		echo"Ingrese opcion: ";
 		(int)$Variable = trim(fgets(STDIN));
 		switch($Variable){
